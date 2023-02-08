fff9768aa4e194223726a4019853c8f670691a42
keerthana121102 <101855404+keerthana121102@users.noreply.github.com>
2023-02-08 23:05:45 +0530

    .

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ app.use(express.json());
 app.use("/user", userRouter);
 app.use("/posts",postRouter);
 
-mongoose.connect("mongodb://localhost:27017/test")
+console.log("Server started")
+
+const url="mongodb://admin:admin@ac-2mob2r2-shard-00-00.qpaicwi.mongodb.net:27017,ac-2mob2r2-shard-00-01.qpaicwi.mongodb.net:27017,ac-2mob2r2-shard-00-02.qpaicwi.mongodb.net:27017/?ssl=true&replicaSet=atlas-4mcaoh-shard-0&authSource=admin&retryWrites=true&w=majority"
+
+mongoose.connect(url)
 .then(() =>
 app.listen(5000, () =>
   console.log("Connection Succesfull  & Listening to localhost Port 5000")
